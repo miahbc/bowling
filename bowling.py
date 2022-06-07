1c3142cd930840de37becd392fefdaa841366e23
Miah Clay <miahb88@gmail.com>
2022-06-07 10:40:03 -0400

    working

--- bowling.py
+++ bowling.py
@@ -1,3 +1,6 @@
+from re import L
+
+
 class Game:
 
     def __init__(self,frames=0,players=[]):
@@ -17,3 +20,15 @@ class Game:
             return "X"
         elif (self.score1 + self.score2) == 10:
             return "/"
+
+class Frame:
+    def __init__(self,balls):
+         self.balls = balls
+         # example: [3,7] is a spare
+         # example: [10] is a strike
+
+    def is_strike(self):
+        return self.balls[0] == 10
+
+    def is_spare(self):
+        return len(self.balls >1 and self.balls[0] + self.balls[1] == 10)
